070dcc5dac99da989bcd451233655404fc1fb436
Everton <end34@bitbuket.org>
2020-09-04 17:31:04 -0300

    ajustando model Periodo e criando periosoService

--- src/app/service/periodo/periodo.service.ts
+++ src/app/service/periodo/periodo.service.ts
@@ -1,9 +1,49 @@
-import { Injectable } from '@angular/core';
+import { Periodo } from "./../../models/periodo";
+import { Injectable } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { MenssagemService } from "../menssagem/menssagem.service";
+import { Plugins } from "@capacitor/core";
+
+const { Storage } = Plugins;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PeriodoService {
+  private PERIODO_STORAGE: string = "periodos";
+  private idCurrent: number = 0;
+  private periodos: Periodo[] = [];
+  private platform: Platform;
 
-  constructor() { }
+  constructor(platform: Platform, private menssagemService: MenssagemService) {
+    this.platform = platform;
+  }
+
+  async findAll(): Promise<Periodo[]> {
+    try {
+      const periodosJson = await Storage.get({ key: this.PERIODO_STORAGE });
+      var dadosPeriodos = JSON.parse(periodosJson.value);
+
+      console.log(dadosPeriodos);
+
+      if (dadosPeriodos) {
+        const { lastId, values } = dadosPeriodos[0];
+        this.idCurrent = lastId;
+        this.periodos = values;
+      }
+
+      return this.getPeriodosAtivos();
+    } catch (error) {
+      this.menssagemService.error("Falha ao buscar Clientes!");
+      console.error("FindAll failed " + error);
+    }
+  }
+
+  private getPeriodosAtivos(): Periodo[] {
+    var periodosAtivos: Periodo[] = this.periodos.filter(
+      (periodo) => periodo.isAtivo
+    );
+
+    return periodosAtivos;
+  }
 }
